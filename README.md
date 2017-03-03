280e0f54d9f99dcb2d38dfa4a6287e684d866908
LanikSJ <laniksj@gmail.com>
2017-03-03 13:52:34 -0800

    Cosmetic fixes in Shell Script and update README.md

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 aws cli ``1.11.24`` or higher:
 [AWS CLI User Guide](http://docs.aws.amazon.com/cli/latest/userguide/installing.html)
 
+Use ``update_awscli.sh`` script to install / update AWS CLI if yours is out of date.
+
 ##Usage
 
 ```
@@ -29,3 +31,6 @@ Output:
     "Arn": "arn:aws:sts::1234567890:user/UserId"
 }
 ```
+## Bugs
+
+Please report any bugs or issues you find.  Thanks!
--- awscli-update.sh
+++ awscli-update.sh
@@ -3,57 +3,57 @@
 # Install / Update Function
 update_awscli() {
   case `uname` in
-  "Darwin" )
-  	type brew
-  	if [ $? != 0 ]
-  	then
-  		echo "brew not found. Please install at http://brew.sh"
-  		exit 1
-  	else
-  		echo "brew already installed..."
-  	fi
+    "Darwin" )
+      type brew
+      if [ $? != 0 ]
+      then
+        echo "brew not found. Please install at http://brew.sh"
+        exit 1
+      else
+        echo "brew already installed..."
+      fi
 
-    type python3
-  	if [ $? != 0 ]
-  	then
-  		echo "python3 not found. Installing.."
-  		brew install python3
-  		if [ $? != 0 ]
-  		then
-  			echo "Failed to install python3.."
-  			exit 1
-  		fi
-  	else
-  		echo "python3 already installed..."
-  	fi
+      type python3
+      if [ $? != 0 ]
+      then
+        echo "python3 not found. Installing.."
+        brew install python3
+        if [ $? != 0 ]
+        then
+          echo "Failed to install python3.."
+          exit 1
+        fi
+      else
+        echo "python3 already installed..."
+      fi
 
-  	type pip3
-  	if [ $? != 0 ]
-  	then
-  		echo "pip3 not found. Please correct."
-  		exit 1
-  	else
-  		echo "pip3 already installed..."
-  	fi
+      type pip3
+      if [ $? != 0 ]
+      then
+        echo "pip3 not found. Please correct."
+        exit 1
+      else
+        echo "pip3 already installed..."
+      fi
 
-    pip3 show awscli
-    if [ $? != 0 ]
-    then
-      echo "You're not using pip. Installing using Brew... "
-      brew install awscli
-    else
-      echo "awscli already installed using pip, upgrading..."
-      pip3 install -U awscli
-    fi
-  ;;
-  Linux )
-  	echo  "For Linux systems, please manually install / update AWS CLI. "
-  	exit 1
-  ;;
-  * )
-  	echo `uname` "is an unsupported OS. Please manually install  / update AWS CLI. "
-  	exit 1
-  ;;
+      pip3 show awscli
+      if [ $? != 0 ]
+      then
+        echo "You're not using pip. Installing using Brew... "
+        brew install awscli
+      else
+        echo "awscli already installed using pip, upgrading..."
+        pip3 install -U awscli
+      fi
+    ;;
+    Linux )
+      echo  "For Linux systems, please manually install / update AWS CLI. "
+      exit 1
+    ;;
+    * )
+      echo `uname` "is an unsupported OS. Please manually install  / update AWS CLI. "
+      exit 1
+    ;;
   esac
 }
 
@@ -61,7 +61,7 @@ update_awscli() {
 AWSVERSION=$(aws --version |awk -F"/" '{print $2}' |sed 's/ Python//')
 
 if [[ -z $AWSVERSION ]]; then
-  echo "AWS CLI isn't installed. "
+  echo "AWS CLI isn't installed. Installing ... "
   update_awscli
 fi
 
