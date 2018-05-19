c871c8d2a5315282600752868194af55823bf0bb
LanikSJ <laniksj@gmail.com>
2018-05-18 20:40:17 -0700

    We Want to Export the Variables

--- awscli.sh
+++ awscli.sh
@@ -59,8 +59,8 @@ awscli() {
 }
 
 # Variables
-AWSVERSION=$(aws --version | awk -F"/" '{print $2}' |sed 's/ Python//')
-AWSCURRENT=$(curl -sSL https://api.github.com/repos/aws/aws-cli/tags | jq -r '.[0].name')
+export AWSVERSION=$(aws --version | awk -F"/" '{print $2}' | sed 's/ Python//')
+export AWSCURRENT=$(curl -sSL https://api.github.com/repos/aws/aws-cli/tags | jq -r '.[0].name')
 
 if [[ -z $AWSVERSION ]]; then
   echo "AWS CLI isn't installed. Installing ... "
