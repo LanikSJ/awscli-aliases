5c7cd07906c048bb247eac901e3022f6fce82686
LanikSJ <laniksj@gmail.com>
2018-09-15 11:21:33 -0700

    Make All if Statements One Line

--- awscli.sh
+++ awscli.sh
@@ -3,26 +3,16 @@
 
 # Install / Update Function
 
-if [[ uname == "Darwin" ]]; then
-  echo "Make sure you have Brew and Python3 Installed"
-elif [[ uname == "Ubuntu" ]]; then
-  echo "Make sure you have Brew and Python3 Installed"
-elif [[ uname == "RedHat" ]] || [[ uname == "CentOS" ]]; then
-  echo "Make sure you have Brew and Python3 Installed"
-fi
+if [[ uname == "Darwin" ]]; then /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && brew install jq python3; fi
+
+if [[ uname == "Ubuntu" ]]; then apt-get install python3 jq; fi
+
+if [[ uname == "RedHat" ]] || [[ uname == "CentOS" ]]; then yum -y install python3 jq; fi
 
 # Variables
 export AWSVERSION=$(aws --version | awk -F"/" '{print $2}' | sed 's/ Python//')
 export AWSCURRENT=$(curl -sSL https://api.github.com/repos/aws/aws-cli/tags | jq -r '.[0].name')
 
-if [[ -z $AWSVERSION ]]; then
-  pip3 install -U awscli
-else
-  echo "Your AWS CLI version is: $AWSVERSION"
-fi
+if [[ -z $AWSVERSION ]]; then pip3 install -U awscli; else echo "Your AWS CLI version is: $AWSVERSION"; fi
 
-if [[ $AWSVERSION < "$AWSCURRENT" ]]; then
-  pip3 install -U awscli
-else
-  echo "Your version of AWS CLI is up to date! Nothing to do. "
-fi
+if [[ $AWSVERSION < "$AWSCURRENT" ]]; then pip3 install -U awscli; else echo "Your version of AWS CLI is up to date! Nothing to do. "; fi
