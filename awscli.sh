9a325e0d5fc064249b22e1b6e57e2f164cd654c2
LanikSJ <laniksj@gmail.com>
2018-09-15 10:55:38 -0700

    Get Rid off All Case Statements

--- awscli.sh
+++ awscli.sh
@@ -2,36 +2,27 @@
 # This script will install/Update AWS CLI
 
 # Install / Update Function
-awscli() {
-  case $(uname) in
-    "Darwin" )
-      # Variables
-      export AWSVERSION=$(aws --version | awk -F"/" '{print $2}' | sed 's/ Python//')
-      export AWSCURRENT=$(curl -sSL https://api.github.com/repos/aws/aws-cli/tags | jq -r '.[0].name')
 
-      if [[ -z $AWSVERSION ]]; then
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        brew install jq python3
-        pip3 install -U awscli
-      else
-        echo "Your AWS CLI version is: $AWSVERSION"
-      fi
+if [[ uanme !== "Darwin" ]]; then
+  echo "This script is only designed for macOS X"
+fi
 
-      if [[ $AWSVERSION < "$AWSCURRENT" ]]; then
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        brew install jq python3
-        pip3 install -U awscli
-      else
-        echo "Your version of AWS CLI is up to date! Nothing to do. "
-      fi
-      ;;
-    Linux )
-      echo "For Linux systems, please manually install / update AWS CLI. "
-      exit 1
-      ;;
-    * )
-      echo "$(uname) is an unsupported OS. Please manually install / update AWS CLI. "
-      exit 1
-      ;;
-  esac
-}
+# Variables
+export AWSVERSION=$(aws --version | awk -F"/" '{print $2}' | sed 's/ Python//')
+export AWSCURRENT=$(curl -sSL https://api.github.com/repos/aws/aws-cli/tags | jq -r '.[0].name')
+
+if [[ -z $AWSVERSION ]]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew install jq python3
+  pip3 install -U awscli
+else
+  echo "Your AWS CLI version is: $AWSVERSION"
+fi
+
+if [[ $AWSVERSION < "$AWSCURRENT" ]]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew install jq python3
+  pip3 install -U awscli
+else
+  echo "Your version of AWS CLI is up to date! Nothing to do. "
+fi
