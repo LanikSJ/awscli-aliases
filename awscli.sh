3b87351f5be4d5c6ce2e5b91f69653b2990aa32c
LanikSJ <laniksj@gmail.com>
2018-09-15 11:07:41 -0700

    Account for Other OSes

--- awscli.sh
+++ awscli.sh
@@ -3,8 +3,13 @@
 
 # Install / Update Function
 
-if [[ uname != "Darwin" ]]; then
-  echo "This script is only designed for macOS X"
+if [[ uname == "Darwin" ]]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew install jq python3
+elif [[ uname == "Ubuntu" ]]; then
+  apt-get install python3 jq
+elif [[ uname == "RedHat" ]] || [[ uname == "CentOS" ]]; then
+  yum -y install python3 jq
 fi
 
 # Variables
@@ -12,16 +17,12 @@ export AWSVERSION=$(aws --version | awk -F"/" '{print $2}' | sed 's/ Python//')
 export AWSCURRENT=$(curl -sSL https://api.github.com/repos/aws/aws-cli/tags | jq -r '.[0].name')
 
 if [[ -z $AWSVERSION ]]; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew install jq python3
   pip3 install -U awscli
 else
   echo "Your AWS CLI version is: $AWSVERSION"
 fi
 
 if [[ $AWSVERSION < "$AWSCURRENT" ]]; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew install jq python3
   pip3 install -U awscli
 else
   echo "Your version of AWS CLI is up to date! Nothing to do. "
