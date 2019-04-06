5a50466ae70cfd082976c106b3eb57247edf7b44
LanikSJ <laniksj@gmail.com>
2019-04-06 15:38:16 -0700

    Update Script To Ignore Brew Install

--- awscli.sh
+++ awscli.sh
@@ -3,7 +3,7 @@
 
 # Install / Update Function
 
-if [[ uname == "Darwin" ]]; then which brew >/dev/null && if [ "$?" = "0" ]; then /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; fi && brew install jq python3; fi
+if [[ uname == "Darwin" ]]; then which brew >/dev/null && if [ "$?" != "0" ]; then /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; fi && brew install jq python3; fi
 
 if [[ uname == "Ubuntu" ]]; then apt-get install python3 jq; fi
 
@@ -11,9 +11,7 @@ if [[ uname == "RedHat" ]] || [[ uname == "CentOS" ]]; then yum -y install pytho
 
 # Variables
 export AWSVERSION=$(aws --version | awk -F"/" '{print $2}' | sed 's/ Python//')
-export AWSCURRENT=$(curl -sSL https://api.github.com/repos/aws/aws-cli/tags | jq -e -r '.[0].name')
-
-if [[ $AWSCURRENT == *"dev"* ]]; then export AWSCURRENT=$(curl -sSL https://api.github.com/repos/aws/aws-cli/tags | jq -e -r '.[1].name'); fi
+export AWSCURRENT=$(curl -sSL https://api.github.com/repos/aws/aws-cli/tags | jq -e -r '.[1].name')
 
 if [[ -z $AWSVERSION ]]; then pip3 install -U awscli; else echo "Your AWS CLI version is: $AWSVERSION"; fi
 
